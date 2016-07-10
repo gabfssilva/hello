b8407044284740b6e0c0e18e654779b77f8ac50d
Gabriel <gabfssilva@gmail.com>
2016-07-10 20:25:49 -0300

    updating jar

--- src/main/java/hello/java/Main.java
+++ src/main/java/hello/java/Main.java
@@ -15,7 +15,7 @@ public class Main {
         ipAddress(appInterface);
         port(appPort);
 
-        get("/hello-java/hello", (req, resp) -> format("hello-scala, world! running on %s:%d", appInterface, appPort));
+        get("/hello-java/hello", (req, resp) -> format("hello-java, world! running on %s:%d", appInterface, appPort));
         get("/hello-java/health", (req, resp) -> "ok!");
         get("/loaderio-93f4cb143cf6225704731e0dfc39c59a/", (req, resp) -> "loaderio-93f4cb143cf6225704731e0dfc39c59a");
     }
